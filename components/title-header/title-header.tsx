895f7af08cd42be8389f2eafdc5051ed4c4e0253
samosaboy <varshney.nikunj@gmail.com>
2019-06-29 14:12:48 -0400

    (CHORE) Fix buttons

--- components/title-header/title-header.tsx
+++ components/title-header/title-header.tsx
@@ -14,17 +14,17 @@ class TitleHeader extends React.Component<ITitleHeaderProps> {
         return (
             <div className={'title-header flex items-center z-999'}>
                 <div className={'container flex items-center justify-between'}>
-                    <span className={'f4'}>
+                    <span className={'f4 black'}>
                         {this.props.tagStore.activeTag
                             ? this.props.tagStore.activeTag.name
                             : 'home'}
                     </span>
                     <div className={'flex'}>
-                        <a className="f5 link dim br3 ph3 pv2 dib white bg-black mr2" href="#0">
+                        <a className="f6 link dim br2 ph3 pv2 dib white bg-green mr2" href="#0">
                             Login
                         </a>
                         <Link route={'/settings'}>
-                            <a className="f5 link dim br3 ph3 pv2 dib white bg-black">Set ID</a>
+                            <a className="f6 link dim br2 ph3 pv2 dib white bg-green mr2">Set ID</a>
                         </Link>
                     </div>
                 </div>
