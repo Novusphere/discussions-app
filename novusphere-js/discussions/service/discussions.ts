31578598799715f041943f8322b637cf14a21fa7
asphyxia <asphyxia@novusphere.io>
2019-07-08 23:14:18 +0000

    add vote() method

--- novusphere-js/discussions/service/discussions.ts
+++ novusphere-js/discussions/service/discussions.ts
@@ -9,18 +9,30 @@ export default class DiscussionsService {
         this.cursorId = 0;
     }
 
-    async makePost(p: Post): Promise<string> {
+    async vote(uuid: string, value: number): Promise<string> {
+        return await eos.transact({
+            contract: "discussionsx",
+            name: "vote",
+            data: {
+                voter: eos.auth.accountName,
+                uuid: uuid,
+                value: value
+            }
+        });
+    }
+
+    async post(p: Post): Promise<string> {
         if (p.chain != 'eos') throw new Error('Unknown chain');
 
         const tags = new Set();
-        [ p.sub, ...p.tags ].forEach( t => tags.add(t.toLowerCase()));
+        [p.sub, ...p.tags].forEach(t => tags.add(t.toLowerCase()));
 
         const mentions = new Set();
         p.mentions.forEach(u => mentions.add(u));
 
-        const metadata : PostMetaData = {};
+        const metadata: PostMetaData = {};
         if (p.title) metadata.title = p.title;
-        if (p.attachment.value)  metadata.attachment = p.attachment;
+        if (p.attachment.value) metadata.attachment = p.attachment;
         if (p.pub && p.sig) {
             metadata.pub = p.pub;
             metadata.sig = p.sig;
@@ -50,7 +62,7 @@ export default class DiscussionsService {
         query = await nsdb.search(0, { threadUuid: op.threadUuid, sub: op.sub }, null);
 
         this.cursorId = query.cursorId;
-        
+
         let posts = query.payload.map(o => Post.fromDbObject(o));
         let thread = new Thread();
         thread.init(posts);
@@ -61,8 +73,8 @@ export default class DiscussionsService {
         const query = await nsdb.search(0, {
             "sub": { "$in": _subs }
         }, {
-            "createdAt": -1
-        });
+                "createdAt": -1
+            });
 
         this.cursorId = query.cursorId;
         return query.payload.map(o => Post.fromDbObject(o));
@@ -70,10 +82,10 @@ export default class DiscussionsService {
 
     async getPostsForTags(_tags: string[]): Promise<Post[]> {
         const query = await nsdb.search(0, {
-            "tags": { "$in": [ 'test' ] }
+            "tags": { "$in": ['test'] }
         }, {
-            "createdAt": -1
-        });
+                "createdAt": -1
+            });
 
         this.cursorId = query.cursorId;
         let posts = query.payload.map(o => Post.fromDbObject(o));
