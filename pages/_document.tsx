95da7085cf5ff03d0968635a15892bd7814a9cdc
samosaboy <varshney.nikunj@gmail.com>
2019-11-10 15:11:19 -0500

    Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,12 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+                    <link rel="manifest" href="/static/site.webmanifest" />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ffffff" />
                 </Head>
                 <body style={{ margin: 0 }}>
                     <Main />
