0e0f415e44f2415b6f06d2a411a18c562300e650
Nikunj Varshney <varshney.nikunj@gmail.com>
2020-02-19 18:46:24 -0500

    fix express route

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, './dist')))
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
+
+const port = process.env.PORT || 3001
+app.listen(port)
+
+console.log('App is listening on port ' + port)
