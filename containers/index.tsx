4f6254061ff0d9c37a2d3e222a3a58c89f7aaafe
Nikunj Varshney <varshney.nikunj@gmail.com>
2020-02-19 19:45:38 -0500

    add back dist to gitignore

--- containers/index.tsx
+++ containers/index.tsx
@@ -3,8 +3,6 @@ import * as ReactDOM from 'react-dom'
 import 'antd/dist/antd.less'
 import '../assets/main.scss'
 
-import serviceWorker from '@containers/serviceWorker'
 import App from '@containers/app'
 
 ReactDOM.render(<App />, document.getElementById('app'))
-serviceWorker()
