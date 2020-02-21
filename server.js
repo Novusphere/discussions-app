326c83577a1ec406e48feff2c10213b8e06ac419
Nikunj Varshney <varshney.nikunj@gmail.com>
2020-02-20 23:20:16 -0500

    change port to 3000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 app.listen(port)
 
 console.log('App is listening on port ' + port)
