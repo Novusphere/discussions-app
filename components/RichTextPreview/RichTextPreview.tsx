13726dccbf90f0b6ea804306c3c4d2d3a2fb401c
samosaboy <varshney.nikunj@gmail.com>
2020-03-13 19:29:28 -0400

    fix #263

--- components/RichTextPreview/RichTextPreview.tsx
+++ components/RichTextPreview/RichTextPreview.tsx
@@ -75,8 +75,8 @@ const RtLink: FunctionComponent<any> = ({ children, href, index }) => {
                                 <a
                                     href={href}
                                     className={'pointer'}
-                                    onClick={() => openInNewTab(href)}
                                     title={'Open image in new tab'}
+                                    target={'_blank'}
                                 >
                                     <LazyLoadImage alt={'Viewing image'} src={href} effect="blur" />
                                 </a>
@@ -134,7 +134,13 @@ const RtLink: FunctionComponent<any> = ({ children, href, index }) => {
     if (!getEmbed || index > LINK_LIMIT) {
         return (
             <object>
-                <a data-indexer-set="true" data-index={index} href={href} title={`Open ${href}`}>
+                <a
+                    data-indexer-set="true"
+                    data-index={index}
+                    href={href}
+                    title={`Open ${href}`}
+                    target={'_blank'}
+                >
                     <object>{children}</object>
                 </a>
             </object>
