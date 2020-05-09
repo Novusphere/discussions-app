9b22902a679631971c581a93658cc6c1d7477f71
samosaboy <varshney.nikunj@gmail.com>
2020-05-09 13:17:49 -0400

    fix #314

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -12,6 +12,7 @@ import styles from './Editor.module.scss'
 import { useCallback, useState } from 'react'
 import { useMemo } from 'react'
 import { generateUuid } from '@utils'
+import { useLocation } from 'react-router-dom'
 
 interface IEditorProps {
     onChange: (html: any) => void // passed in via spread (bind) in form.tsx
@@ -32,6 +33,7 @@ interface IEditorState {
 const prefix = 'toolbar-'
 
 const CustomToolbar = ({ addTip, toolbarId }) => {
+    const location = useLocation()
     const [amount, setAmount] = useState(0)
     const [symbol, setSymbol] = useState('')
 
@@ -172,58 +174,64 @@ const CustomToolbar = ({ addTip, toolbarId }) => {
                     </svg>
                 </button>
             </span>
-            <span className="ql-formats">
-                <Popconfirm
-                    icon={<Icon type="dollar" />}
-                    placement="topLeft"
-                    title={
-                        <div>
-                            <span className={'flex flex-row items-center justify-between mb1'}>
-                                Amount
-                                <InputNumber
-                                    min={0}
-                                    onChange={onAmountChange}
-                                    className={styles.tipInput}
-                                />
-                            </span>
-                            <span className={'flex flex-row items-center justify-between'}>
-                                Token
-                                <Select className={styles.tipInput} onChange={onTokenChange}>
-                                    {Object.keys(tokens).length > 0
-                                        ? Object.keys(tokens).map(token => (
-                                              <Option value={token} key={token}>
-                                                  {token}
-                                              </Option>
-                                          ))
-                                        : null}
-                                </Select>
-                            </span>
-                        </div>
-                    }
-                    onConfirm={onConfirm}
-                    okText="Add Tip"
-                    cancelText="Close"
-                >
-                    <button type="button" className="ql-inline-tip" onMouseDown={e => e.preventDefault()}>
-                        <svg
-                            aria-hidden="true"
-                            focusable="false"
-                            data-prefix="fas"
-                            data-icon="comment-dollar"
-                            role="img"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512"
-                            className="svg-inline--fa fa-comment-dollar fa-w-16 fa-2x"
+            {!location.pathname.startsWith('/new') && (
+                <span className="ql-formats">
+                    <Popconfirm
+                        icon={<Icon type="dollar" />}
+                        placement="topLeft"
+                        title={
+                            <div>
+                                <span className={'flex flex-row items-center justify-between mb1'}>
+                                    Amount
+                                    <InputNumber
+                                        min={0}
+                                        onChange={onAmountChange}
+                                        className={styles.tipInput}
+                                    />
+                                </span>
+                                <span className={'flex flex-row items-center justify-between'}>
+                                    Token
+                                    <Select className={styles.tipInput} onChange={onTokenChange}>
+                                        {Object.keys(tokens).length > 0
+                                            ? Object.keys(tokens).map(token => (
+                                                  <Option value={token} key={token}>
+                                                      {token}
+                                                  </Option>
+                                              ))
+                                            : null}
+                                    </Select>
+                                </span>
+                            </div>
+                        }
+                        onConfirm={onConfirm}
+                        okText="Add Tip"
+                        cancelText="Close"
+                    >
+                        <button
+                            type="button"
+                            className="ql-inline-tip"
+                            onMouseDown={e => e.preventDefault()}
                         >
-                            <path
-                                fill="currentColor"
-                                d="M256 32C114.62 32 0 125.12 0 240c0 49.56 21.41 95.01 57.02 130.74C44.46 421.05 2.7 465.97 2.2 466.5A7.995 7.995 0 0 0 8 480c66.26 0 115.99-31.75 140.6-51.38C181.29 440.93 217.59 448 256 448c141.38 0 256-93.12 256-208S397.38 32 256 32zm24 302.44V352c0 8.84-7.16 16-16 16h-16c-8.84 0-16-7.16-16-16v-17.73c-11.42-1.35-22.28-5.19-31.78-11.46-6.22-4.11-6.82-13.11-1.55-18.38l17.52-17.52c3.74-3.74 9.31-4.24 14.11-2.03 3.18 1.46 6.66 2.22 10.26 2.22h32.78c4.66 0 8.44-3.78 8.44-8.42 0-3.75-2.52-7.08-6.12-8.11l-50.07-14.3c-22.25-6.35-40.01-24.71-42.91-47.67-4.05-32.07 19.03-59.43 49.32-63.05V128c0-8.84 7.16-16 16-16h16c8.84 0 16 7.16 16 16v17.73c11.42 1.35 22.28 5.19 31.78 11.46 6.22 4.11 6.82 13.11 1.55 18.38l-17.52 17.52c-3.74 3.74-9.31 4.24-14.11 2.03a24.516 24.516 0 0 0-10.26-2.22h-32.78c-4.66 0-8.44 3.78-8.44 8.42 0 3.75 2.52 7.08 6.12 8.11l50.07 14.3c22.25 6.36 40.01 24.71 42.91 47.67 4.05 32.06-19.03 59.42-49.32 63.04z"
-                                className=""
-                            ></path>
-                        </svg>
-                    </button>
-                </Popconfirm>
-            </span>
+                            <svg
+                                aria-hidden="true"
+                                focusable="false"
+                                data-prefix="fas"
+                                data-icon="comment-dollar"
+                                role="img"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                                className="svg-inline--fa fa-comment-dollar fa-w-16 fa-2x"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    d="M256 32C114.62 32 0 125.12 0 240c0 49.56 21.41 95.01 57.02 130.74C44.46 421.05 2.7 465.97 2.2 466.5A7.995 7.995 0 0 0 8 480c66.26 0 115.99-31.75 140.6-51.38C181.29 440.93 217.59 448 256 448c141.38 0 256-93.12 256-208S397.38 32 256 32zm24 302.44V352c0 8.84-7.16 16-16 16h-16c-8.84 0-16-7.16-16-16v-17.73c-11.42-1.35-22.28-5.19-31.78-11.46-6.22-4.11-6.82-13.11-1.55-18.38l17.52-17.52c3.74-3.74 9.31-4.24 14.11-2.03 3.18 1.46 6.66 2.22 10.26 2.22h32.78c4.66 0 8.44-3.78 8.44-8.42 0-3.75-2.52-7.08-6.12-8.11l-50.07-14.3c-22.25-6.35-40.01-24.71-42.91-47.67-4.05-32.07 19.03-59.43 49.32-63.05V128c0-8.84 7.16-16 16-16h16c8.84 0 16 7.16 16 16v17.73c11.42 1.35 22.28 5.19 31.78 11.46 6.22 4.11 6.82 13.11 1.55 18.38l-17.52 17.52c-3.74 3.74-9.31 4.24-14.11 2.03a24.516 24.516 0 0 0-10.26-2.22h-32.78c-4.66 0-8.44 3.78-8.44 8.42 0 3.75 2.52 7.08 6.12 8.11l50.07 14.3c22.25 6.36 40.01 24.71 42.91 47.67 4.05 32.06-19.03 59.42-49.32 63.04z"
+                                    className=""
+                                ></path>
+                            </svg>
+                        </button>
+                    </Popconfirm>
+                </span>
+            )}
         </div>
     )
 }
